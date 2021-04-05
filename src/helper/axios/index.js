aadaf7587b82c5387e6b3630b0efdbf532b33b64
Gustavo Gonzalez F <gonz.gustavoe@gmail.com>
2021-04-05 23:59:59 +0000

    Merged in feature/servicio-para-peticiones-http (pull request #11)
    Feature/servicio para peticiones http
    
    * Se instalo axios para manejar Http requests
    
    * Se creo un metodo reutilizable para hacer resquest con axios
    
    * Se agrega JSDoc a la funcion axiosHttpRequest
    
    * Modificacion con metodos get y post
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t22-client into feature/servicio-para-peticiones-http
    
    Approved-by: Tomas Guzman

---
+++ src/helper/axios/index.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+export const getHttpRequest = (url, data) => {
+    const config = createAxiosConfig(url, data);
+    config.method = "get";
+
+    return axios(config);
+};
+
+export const postHttpRequest = (url, data) => {
+    const config = createAxiosConfig(url, data);
+    config.method = "post";
+
+    return axios(config);
+};
+
+const createAxiosConfig = (url, data) => {
+    const jwtToken = localStorage.getItem("token");
+
+    const axiosConfig = {
+        url,
+    };
+
+    if (data) {
+        axiosConfig.data = data;
+    }
+
+    if (jwtToken) {
+        axiosConfig.headers = { token: jwtToken };
+    }
+
+    return axiosConfig;
+};
