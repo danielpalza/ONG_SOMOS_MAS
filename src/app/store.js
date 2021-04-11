7c78b01ec3cd390838d7e292cd1949b1d7894821
Agustin Di Nardo <agusdinar96@gmail.com>
2021-04-11 07:13:20 +0000

    Merged in feature/OT22-27-redux-user-storage (pull request #5)
    added userslice with storage in redux
    
    * added userslice with storage in redux
    
    * changed quotes
    
    * changed quotes
    
    * fix conflicts
    
    * resolved conflicts
    
    Approved-by: Tomas Guzman

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../components/counter/counterSlice';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
 import fetchReducer from '../features/fetch/fetchSlice'
+import { persistReducer } from 'redux-persist';
+import counterReducer from '../features/counter/counterSlice';
+import userReducer from '../features/user/userSlice';
+
+const reducers = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    user: persistedReducer,
     fetch: fetchReducer
   },
 });
--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+const persistor = persistStore(store);
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
