be61c724be789dc6d1d275bd35b22bd96abf19b2
Gustavo Gonzalez F <gonz.gustavoe@gmail.com>
2021-05-03 16:51:17 +0000

    Merged in feature/OT22-152-news-page-fix-se-repiten-news (pull request #75)
    Fix se repiten las news
    
    * Se borra el causante del bug
    
    Approved-by: Tomas Guzman

--- src/pages/News/News.js
+++ src/pages/News/News.js
@@ -26,20 +26,6 @@ class NewsPublic extends React.Component {
           <meta charSet="utf-8" />
           <title>Somos Más - Novedades</title>
         </Helmet>
-        <div className="cards">
-          {this.state.data.map(element => (
-            <div key={element.id} className="card">
-              <img src={element.image} className="card-img-top" alt="img" />
-              <div className="card-body">
-                <h5 className="card-title"> {element.name} </h5>
-                <Link to={`/news/${element.id}`}>
-                  <button className="btn btn-primary">Details</button>
-                </Link>
-              </div>
-            </div>
-          ))}
-        </div>
-      
         <main className="page">
           <section className="clean-block about-us">
             <div className="container news-content">
