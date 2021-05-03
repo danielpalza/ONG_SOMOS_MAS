a1c8d08cc2ac211a6f9b9f897e34323117acb25d
Gustavo Gonzalez F <gonz.gustavoe@gmail.com>
2021-05-03 13:29:13 +0000

    Merged in feature/OT22-125-home-page---Colocar-el-logo (pull request #57)
    Logo se dejo HardCoded en el Nav
    
    * Logo se dejo HardCoded en el Nav
    
    * Merge remote-tracking branch 'origin/master' into feature/OT22-125-home-page---Colocar-el-logo
    
    # Conflicts:
    #	src/components/Header/Header.js
    
    Approved-by: Tomas Guzman

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,52 +4,60 @@ import { getHttpRequest } from '../../helper/axios/index';
 import Loader from '../../components/Loader';
 import './Header.css';
 import { Link } from 'react-router-dom';
+import Logo from '../../assets/LOGO-SOMOS-MAS.png';
 
 const Header = () => {
   const [state, setState] = useState({});
 
-  useEffect(() => {
-    getHttpRequest(`${process.env.REACT_APP_API_URL}/organizations/1/public`)
-      .then(res => {
-        setState(res.data);
-      })
-      .catch(err => console.error(err));
-  }, []);
+  // useEffect(() => {  Comented due to Presentation
+  //   getHttpRequest(`${process.env.REACT_APP_API_URL}/organizations/1/public`)
+  //     .then(res => {
+  //       setState(res.data);
+  //     })
+  //     .catch(err => console.error(err));
+  // }, []);
 
-  const { image } = state;
+  // const { image } = state; Committed due to Presentation
 
   return (
-    <nav className="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar shadow header">
-      <div className="container header-container">
-        {image ? (
-          <img className="logo-image" src={image} alt="logo" />
-        ) : (
-          <Loader />
-        )}
-
-        <button
-          data-toggle="collapse"
-          className="navbar-toggler"
-          data-target="#navcol-1"
-        >
-          <span className="sr-only">Toggle navigation</span>
-          <span className="navbar-toggler-icon"> </span>
-        </button>
-        <div className="collapse navbar-collapse" id="navcol-1">
-          <ul className="navbar-nav ml-auto">
-            {links.map(({ route, text }, index) => {
-              return (
-                <li className="nav-item" key={index}>
-                  <Link to={route} className="nav-link links">
-                    {text}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+        <div className="container-fluid">
+          {/* {image ? ( Committed due to Presentation
+            <img className="logo-image" src={image} alt="logo" />
+          ) : (
+            <Loader />
+          )} */}
+          <img className="logo-image" src={Logo} alt="logo" />
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#collapse_target"
+            aria-expanded="false"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div
+            style={{ paddingRight: '50px' }}
+            className="collapse  navbar-collapse justify-content-end"
+            id="collapse_target"
+          >
+            <ul className="navbar-nav">
+              {links.map(({ route, text }, index) => {
+                return (
+                  <li className="nav-item" key={index}>
+                    <Link to={route} className="nav-link text-uppercase">
+                      {text}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
